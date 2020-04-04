12859fda305428fa5e44ccfadc1568c0ff237f6a
Yumin <yumin@deckardtech.com>
2020-04-03 19:25:02 -0700

    master #comment use plus #assign yumin@deckardtech.com

---
+++ basics/plus.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b;
+
+  printf("Please define 2 numbers: \n");
+  scanf("%d %d", &a, &b);
+  printf("%d + %d = %d\n", a, b, a + b);
+
+  return 0;
+}
