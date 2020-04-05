9d0252b25e6cc84f9b9961956b0cd2760f160626
Yumin <yumin@deckardtech.com>
2020-04-05 12:41:14 -0700

    master #comment sum #assign yumin@deckardtech.com

---
+++ basics/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int n = 0;
+  double sum = 0.0;
+
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++)
+  {
+    sum += 1.0 / i;
+  }
+
+  printf("Sum is: %4.2f\n", sum); // what is %4.2f ?
+}
