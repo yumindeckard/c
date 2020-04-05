e6ddafec62b7934bd607be24c734699525ad133f
Yumin <yumin@deckardtech.com>
2020-04-04 23:42:23 -0700

    master #comment is prime #assign yumin@deckardtech.com

---
+++ basics/is_prime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int x;
+  scanf("%d", &x);
+
+  for (int a = 2; a < x; a++)
+  {
+    if (x % a == 0)
+    {
+      printf("no, it is not a prime.\n");
+      return 0;
+    }
+  }
+
+  printf("yes, it is a prime.\n");
+
+  return 0;
+}
