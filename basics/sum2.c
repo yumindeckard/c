60dbe3cf8ad949131413263f20e8a7733a7ff831
Yumin <yumin@deckardtech.com>
2020-04-05 12:44:28 -0700

    master #comment sum2 #assign yumin@deckardtech.com

--- basics/sum2.c
+++ basics/sum2.c
@@ -4,11 +4,13 @@ int main()
 {
   int n = 0;
   double sum = 0.0;
+  double sign = 1.0;
 
   scanf("%d", &n);
   for (int i = 1; i <= n; i++)
   {
-    sum += 1.0 / i;
+    sum += sign / i;
+    sign = -sign;
   }
 
   printf("Sum is: %.2f\n", sum); // what is %4.2f ?
