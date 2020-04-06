025d31ccb72f681d28a5ed874ea68e878d80334f
Yumin <yumin@deckardtech.com>
2020-04-06 15:31:53 -0700

    master #comment greatest common divisor #assign yumin@deckardtech.com

---
+++ basics/greatest-common-divisor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int a, b;
+  int min;
+
+  scanf("%d %d", &a, &b);
+  if (a == b) {
+    printf("GCD is %d\n", a);
+    return 0;
+  } else if (a > b) {
+    min = b;
+  } else {
+    min = a;
+  }
+
+  int ret = 0;
+  for (int i = 1; i <= min; i++) {
+    if (a % i == 0 && b % i == 0) {
+      if (ret < i) {
+        ret = i;
+      }
+    }
+  }
+
+  printf("GCD is %d\n", ret);
+
+  return 0;
+}
