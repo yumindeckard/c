16cb37e7335a279bce6b13405a6563941f619ffc
Yumin <yumin@deckardtech.com>
2020-04-06 15:41:46 -0700

    master #comment gcd 2 #assign yumin@deckardtech.com

--- basics/greatest-common-divisor2.c
+++ basics/greatest-common-divisor2.c
@@ -4,5 +4,12 @@ int main(int argc, char const *argv[]) {
   int a, b;
   scanf("%d %d", &a, &b);
 
+  int t = 0;
+  while (b != 0) {
+    t = a % b;
+    a = b;
+    b = t;
+  }
+  printf("GCD is %d\n", a);
   return 0;
 }
