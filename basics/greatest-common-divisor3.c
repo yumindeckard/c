6367880db6a5e44191b152df901b3bb6affd76ff
Yumin <yumin@deckardtech.com>
2020-04-10 16:31:07 -0700

    master #comment gcd 3 #assign yumin@deckardtech.com

---
+++ basics/greatest-common-divisor3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int getGCD(int a, int b) {
+  int t = 0;
+  while (b != 0) {
+    t = a % b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+int main(int argc, char const *argv[]) {
+  int m, n;
+  scanf("%d %d", &m, &n);
+
+  int gcd = getGCD(m, n);
+
+  m = m / gcd;
+  n = n / gcd;
+
+  printf("%d/%d\n", m, n);
+
+  return 0;
+}
