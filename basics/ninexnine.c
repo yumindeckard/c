9290e39a09db22b49e7ad2210445419bc7bfb029
Yumin <yumin@deckardtech.com>
2020-04-11 09:49:22 -0700

    master #comment use t #assign yumin@deckardtech.com

--- basics/ninexnine.c
+++ basics/ninexnine.c
@@ -3,10 +3,10 @@
 int main(int argc, char const *argv[]) {
   for (int i = 1; i <= 9; i++) {
     for (int j = 1; j <= i; j++) {
-      printf("%d*%d=%d ", j, i, j * i);
-      if (j * i < 10 && j > 1) {
-        printf(" ");
-      }
+      printf("%d*%d=%d\t", j, i, j * i);
+      // if (j * i < 10 && j > 1) {
+      //   printf(" ");
+      // }
       if (j == i) {
         printf("\n");
       }
