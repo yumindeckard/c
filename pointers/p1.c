be393a80701512749cc4ab7c201a0b51fc0a4608
Yumin <yumin@deckardtech.com>
2020-04-16 16:17:10 -0700

    master #comment pointer #assign yumin@deckardtech.com

--- pointers/p1.c
+++ pointers/p1.c
@@ -6,10 +6,12 @@ int main(int argc, char const *argv[]) {
   int i = 9;
   printf("i addr: %p\n", &i);
   f(&i);
+  printf("i new value: %d\n", i);
   return 0;
 }
 
 void f(int *p) {
   printf("p is: %p\n", p);
-  printf("p target value is %d\n", *p);
+  printf("p target original value is %d\n", *p);
+  *p = 26;
 }
