99d190dd25f515c786f447e78bb9d5c44ce7e16c
Yumin <yumin@deckardtech.com>
2020-04-18 17:59:32 -0700

    master #comment add more info to p2-swap #assign yumin@deckardtech.com

--- pointers/p2-swap.c
+++ pointers/p2-swap.c
@@ -5,6 +5,7 @@ void swap(int *p1, int *p2);
 int main(int argc, char const *argv[]) {
   int a = 1;
   int b = 2;
+  printf("original ab: a: %d b: %d\n", a, b);
   swap(&a, &b);
   printf("swap ab: a: %d b: %d\n", a, b);
   return 0;
